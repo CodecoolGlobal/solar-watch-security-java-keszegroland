d92812a6ae039b06e828d35e285160bfc7434411
rolandkeszeg <78346746+keszegroland@users.noreply.github.com>
2024-07-16 13:17:53 +0200

    dockerize the project

--- solar-watch/src/main/java/com/codecool/solarwatch/controller/SolarWatchController.java
+++ solar-watch/src/main/java/com/codecool/solarwatch/controller/SolarWatchController.java
@@ -1,29 +1,24 @@
 package com.codecool.solarwatch.controller;
 
-import com.codecool.solarwatch.model.SunriseSunset;
 import com.codecool.solarwatch.service.SolarWatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SolarWatchController {
     private final SolarWatchService solarWatchService;
 
+    @Autowired
     public SolarWatchController(SolarWatchService solarWatchService) {
         this.solarWatchService = solarWatchService;
     }
 
     @GetMapping("/solarwatch")
-    public String getSolarWatchReport(@RequestParam(defaultValue = "Budapest") String city, @RequestParam(defaultValue = "#{T(java.time.LocalDate).now().toString()}") String date, Model model) {
-        SunriseSunset solarWatchRep = solarWatchService.getSolarWatchReport(city, date);
-        model.addAttribute("city", city);
-        model.addAttribute("date", date);
-        model.addAttribute("solarWatchRep", solarWatchRep);
-        return "solarWatch";
+    public ResponseEntity<?> getSolarWatchReport(@RequestParam(defaultValue = "Budapest") String city, @RequestParam(defaultValue = "#{T(java.time.LocalDate).now().toString()}") String date) {
+        return ResponseEntity.ok(solarWatchService.getSolarWatchReport(city, date));
     }
+
 }
